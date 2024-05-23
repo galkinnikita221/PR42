ecb8217dbe148d0abf91c9ec4bd4455139a503da
student-a310 <student-a310@PC-A310-9>
2024-05-23 13:02:22 +0500

    Разработанные модели данных. Galkin

---
+++ ShopContent_Galkin/Modell/Items.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ShopContent_Galkin.Modell
+{
+	public class Items : INotifyPropertyChanged
+	{
+		private int id;
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName] string prop = "")
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(prop));
+		}
+		public int Id
+		{
+			get { return id; }
+			set
+			{
+				id = value;
+				OnPropertyChanged("Id");
+			}
+		}
+		private string name;
+		public string Name
+		{
+			get { return name; }
+			set
+			{
+				name = value;
+				OnPropertyChanged("Name");
+			}
+		}
+		private double price;
+		public double Price
+		{
+			get { return price; }
+			set
+			{
+				price = value;
+				OnPropertyChanged("Price");
+			}
+		}
+		private Categorys category;
+		public Categorys Category
+		{
+			get { return category; }
+			set
+			{
+				category = value;
+				OnPropertyChanged("Category");
+			}
+		}
+	}
+}
